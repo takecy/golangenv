03efd5a2293f659601e16687949d4823237c0789
takeshi yamashita <yamashita_takeshi@cyberagent.co.jp>
2015-04-17 19:01:17 +0900

    setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#golang
+wget "https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz"
+tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOROOT=$HOME/go
+export PATH=$PATH:$GOROOT/bin
+
+#git
+sudo yum intall git
